b36297be40767c09c78cfbe59e6d4e5d1c71000b
Efimov <efimovgym91@gmail.com>
2023-05-01 20:15:24 +0300

    Actual, expected

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
